832c05b43cab30f82bfff926175f27687e1a3591
Muskan Poddar <poddarmuskan888@gmail.com>
2025-08-16 19:45:27 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'counter_view.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-    
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: CounterView());
   }
 }
